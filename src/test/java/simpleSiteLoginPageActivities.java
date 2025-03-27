1fc293f8c5b2bbe38c1704fdb9327d709efc4dda
Gnanaprasad R <gnanaprasad.r@dxc.com~>
2025-03-27 10:38:40 +0530

    Removed Thread.sleep wherever not necessary

--- src/test/java/simpleSiteLoginPageActivities.java
+++ src/test/java/simpleSiteLoginPageActivities.java
@@ -24,8 +24,6 @@ public class simpleSiteLoginPageActivities {
 
         driver.findElement(By.linkText("Forgot your password?")).click();
 
-        Thread.sleep(1000);
-
         driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
         driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@rsa.com");
         driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
@@ -34,9 +32,9 @@ public class simpleSiteLoginPageActivities {
         driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
         System.out.println(driver.findElement(By.cssSelector("form p")).getText());
         driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
-        Thread.sleep(1000);
         driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
         driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+        Thread.sleep(1000);
         driver.findElement(By.id("chkboxOne")).click();
         driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
         driver.navigate().back();
